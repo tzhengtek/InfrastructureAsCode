db28b6eb94032066d27110b62fb6322cd03cc623
tzheng <timothe.zheng@epitech.eu>
2025-12-05 14:47:21 +0100

    feat(terraform): added iam configuration for github and google cloud

---
+++ iam/variables.tf
@@ -0,0 +1,36 @@
+variable "project_id" {
+  type        = string
+  description = "Cloud project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "Region for resources"
+}
+
+// USERS GITHUB // 
+variable "github_admin_users" {
+  type        = list(string)
+  default     = []
+  description = "List of GitHub usernames with admin permission"
+}
+
+variable "github_read_users" {
+  type        = list(string)
+  default     = []
+  description = "List of GitHub usernames with read permission"
+}
+
+variable "gcp_iam_members" {
+  type = map(list(string))
+  default = {
+    "jeremie@jjaouen.com" = ["roles/viewer", "roles/viewer"]
+  }
+  description = "Map of user emails to their IAM roles"
+}
+
+variable "github_iam_members" {
+  type        = map(string)
+  default     = {}
+  description = "Map of user emails to their IAM roles"
+}
---
+++ infrastructure/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "gcs" {
+
+  }
+}
+
+// RESOURCES //
+// --------------------------------------
+resource "google_compute_network" "main" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "main" {
+  name          = "${var.vpc_name}-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.main.id
+}
+// --------------------------------------
+
+module "access" {
+  source     = "./modules/access"
+  project_id = var.project_id
+}
+
+output "vpc_id" {
+  value       = google_compute_network.main.id
+  description = "The ID of the created VPC"
+}
