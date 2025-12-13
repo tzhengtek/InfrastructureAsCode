4ab2f5b272b59fcc581a57555c709307b0b5a997
tzheng <timothe.zheng@epitech.eu>
2025-12-13 17:01:35 +0100

    refactor(google-account): move resources google account at start of google.tf

--- infrastructure/google.tf
+++ infrastructure/google.tf
@@ -1,3 +1,25 @@
+
+resource "google_service_account_iam_binding" "github_action_workload_identity" {
+  service_account_id = google_service_account.github_action.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-action-pool.name}/attribute.repository_owner/${var.github_org}"
+  ]
+}
+resource "google_service_account_iam_binding" "github_action_admin" {
+  service_account_id = google_service_account.github_action.name
+  role               = "roles/iam.serviceAccountAdmin"
+
+  members = [
+    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-action-pool.name}/attribute.repository_owner/${var.github_org}"
+  ]
+}
+resource "google_storage_bucket_iam_member" "terraform_state_access" {
+  bucket = "iac-epitech-storage"
+  role   = "roles/storage.objectAdmin"
+  member = "user:${google_service_account.github_action.account_id}@${var.project_id}.iam.gserviceaccount.com"
+}
 resource "google_iam_workload_identity_pool" "github-action-pool" {
   workload_identity_pool_id = "github-action-pool"
   project                   = var.project_id
@@ -25,27 +47,4 @@ resource "google_service_account" "github_action" {
   account_id   = "github-action-sa"
   display_name = "GitHub Actions Service Account"
   project      = var.project_id
-}
-
-
-resource "google_service_account_iam_binding" "github_action_workload_identity" {
-  service_account_id = google_service_account.github_action.name
-  role               = "roles/iam.workloadIdentityUser"
-
-  members = [
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-action-pool.name}/attribute.repository_owner/${var.github_org}"
-  ]
-}
-resource "google_service_account_iam_binding" "github_action_admin" {
-  service_account_id = google_service_account.github_action.name
-  role               = "roles/iam.serviceAccountAdmin"
-
-  members = [
-    "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github-action-pool.name}/attribute.repository_owner/${var.github_org}"
-  ]
-}
-resource "google_storage_bucket_iam_member" "terraform_state_access" {
-  bucket = "iac-epitech-storage"
-  role   = "roles/storage.objectAdmin"
-  member = "user:${google_service_account.github_action.account_id}@${var.project_id}.iam.gserviceaccount.com"
 }
