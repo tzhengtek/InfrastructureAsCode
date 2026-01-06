08078daaf282b38025c8068f3fb919cd9af0e14e
tzheng <timothe.zheng@epitech.eu>
2026-01-07 00:52:20 +0100

    refactor(file): move kubernetes files at root

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -73,3 +73,32 @@ variable "app_private_key" {
   description = "github installation id"
   sensitive   = true
 }
+
+
+// CLUSTER 
+
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
+}
+
+variable "cluster_name" {
+  default     = "self-hosted"
+  description = "Runner Kubernetes cluster"
+}
+
+variable "arc_runner_name" {
+  default     = "self-hosted"
+  description = "Runner Kubernetes cluster"
+}
