3ad9fd7322fb185224acc6afa971eded68d78e2f
tzheng <timothe.zheng@epitech.eu>
2026-01-08 23:04:00 +0100

    provider helm

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -15,16 +15,18 @@ provider "google" {
 # Automatically fetch GKE credentials for Helm
 data "google_client_config" "default" {}
 
-provider "helm" {
-  kubernetes {
-    host                   = "https://${google_container_cluster.primary.endpoint}"
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-  }
-}
 
 provider "kubernetes" {
   host                   = "https://${google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
+
+provider "helm" {
+  kubernetes = {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  }
+}
+
