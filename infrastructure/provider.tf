ac583a3609bc564ee473094d9b8cd6d89e4ef9f4
tzheng <timothe.zheng@epitech.eu>
2026-01-08 23:06:34 +0100

    provider helm

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -23,7 +23,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = "https://${google_container_cluster.primary.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
